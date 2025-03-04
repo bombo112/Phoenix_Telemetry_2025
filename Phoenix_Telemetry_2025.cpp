153ef5bea31226f6b38ec32fd4dc4d873bd2bb7a
Jens Petersen <jensp99@icloud.com>
2025-03-04 17:07:03 +0100

    Siste Jens oppdatering, ruben takeover

--- Phoenix_Telemetry_2025.cpp
+++ Phoenix_Telemetry_2025.cpp
@@ -54,10 +54,6 @@ int main()
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
     gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
     
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
     // Get a free channel, panic() if there are none
     int chan = dma_claim_unused_channel(true);
